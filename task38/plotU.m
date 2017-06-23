16d632872b346cd72d7cf78349de47f4811d7375
Роман Макаров <glockskat@yandex.ru>
2017-06-23 12:07:15 +0300

    Построение графика скорости в задаче по ОММ для Анастасии

--- task38/plotU.m
+++ task38/plotU.m
@@ -44,39 +44,31 @@ fclose(file);
 
 [X Y] = meshgrid(0:hx:hx * (Nx - 1),0:hy:hy * (Ny - 1));
 
-
-subplot(2,2,1);
+figure;
 axis([0 1 0 2 -1 1]);
-surf(X, Y, U0); 
-
-subplot(2,2,2);
-surf(X, Y, U32); axis([0 1 0 2 -1 1]);
-
-subplot(2,2,3);
-surf(X, Y, U64); axis([0 1 0 2 -1 1]);
-
-subplot(2,2,4);
-surf(X, Y, U128); axis([0 1 0 2 -1 1]);
-
-subplot(2,2,1);
+surf(X, Y, U0);
 title('U(X,Y), T = 0');
 xlabel('X');
 ylabel('Y');
 zlabel('U(X,Y)');
+ 
 
-subplot(2,2,2);
+figure;
+surf(X, Y, U32); axis([0 1 0 2 -1 1]);
 title('U(X,Y), T = 0.025');
 xlabel('X');
 ylabel('Y');
 zlabel('U(X,Y)');
 
-subplot(2,2,3);
+figure
+surf(X, Y, U64); axis([0 1 0 2 -1 1]);
 title('U(X,Y), T = 0.05');
 xlabel('X');
 ylabel('Y');
 zlabel('U(X,Y)');
 
-subplot(2,2,4);
+figure
+surf(X, Y, U128); axis([0 1 0 2 -1 1]);
 title('U(X,Y), T = 0.1');
 xlabel('X');
 ylabel('Y');
